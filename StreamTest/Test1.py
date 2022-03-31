0553cf105a22cc55969cba8ab7d132eb3a11903b
togetherhappy <32669008+togetherhappy@users.noreply.github.com>
2022-03-31 15:00:10 +0800

    Update Test1.py

--- StreamTest/Test1.py
+++ StreamTest/Test1.py
@@ -1,45 +1,20 @@
 import streamlit as st
-import pandas as pd
+import plotly.figure_factory as ff
 import numpy as np
 
-st.title('Uber pickups in NYC')
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# Group data together
+hist_data = [x1, x2, x3]
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-data_load_state = st.text('Loading data...')
-data = load_data(100000)
-data_load_state.text("Done! (using st.cache)")
-title = st.text_input('Movie title', 'Life of Brian')
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+         hist_data, group_labels, bin_size=[.1, .25, .5])
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-product_list = data["base"].unique()
-product_type = st.sidebar.selectbox(
-    "base",
-    product_list
-)
-
-part_data = data[(data["base"] == product_type)]
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(part_data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = part_data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
-
-
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
