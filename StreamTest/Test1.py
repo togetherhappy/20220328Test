52f36af3d349b3ff56290870af3385852a7b5a4a
togetherhappy <32669008+togetherhappy@users.noreply.github.com>
2022-03-31 15:52:15 +0800

    Update Test1.py

--- StreamTest/Test1.py
+++ StreamTest/Test1.py
@@ -34,3 +34,20 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+         hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
