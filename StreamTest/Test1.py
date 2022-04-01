5df4ded2507a9c28a2431d9508472afab97cd2dd
togetherhappy <32669008+togetherhappy@users.noreply.github.com>
2022-04-01 13:05:55 +0800

    Update Test1.py

--- StreamTest/Test1.py
+++ StreamTest/Test1.py
@@ -63,6 +63,15 @@ fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
                  size='petal_length', hover_data=['petal_width'])
 st.plotly_chart(fig, use_container_width=True)
 
+data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20]]
+fig = px.imshow(data,
+                labels=dict(x="Day of Week", y="Time of Day", color="Productivity"),
+                x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                y=['Morning', 'Afternoon', 'Evening']
+               )
+fig.update_xaxes(side="top")
+st.plotly_chart(fig, use_container_width=True)
+
 df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
 df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
 fig = px.pie(df, values='pop', names='country', title='Population of European continent')
